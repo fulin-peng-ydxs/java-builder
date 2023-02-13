01e538e4b5adbf060243d6b44df6a6e331609505
pengfulin <1162855925@qq.com>
2023-02-13 15:01:37 +0800

    v1.3.4  -调整模式构建总线架构

--- builder-core/src/main/java/build/bus/rest/RestAbstractBuildBus.java
+++ builder-core/src/main/java/build/bus/rest/RestAbstractBuildBus.java
@@ -1,4 +1,4 @@
-package build.bus.mvc;
+package build.bus.rest;
 
 import build.builder.meta.BuildCoder;
 import build.bus.exception.BuildBusException;
@@ -9,40 +9,29 @@ import build.source.resolver.meta.BuildSourceResolver;
 import java.io.OutputStream;
 import java.util.List;
 /**
- * Mvc模式构建总线
+ * Rest模式构建总线
  *
  * @author peng_fu_lin
  * 2022-09-07 16:58
  */
-public class MvcSchemaBuildBus extends SchemaBuildBus {
+public abstract class RestAbstractBuildBus extends SchemaBuildBus {
 
-    public MvcSchemaBuildBus(List<BuildCoder<?>> buildCoders, List<BuildResponder> buildResponders, List<BuildSourceResolver<?>> buildSourceResolvers) {
+    public RestAbstractBuildBus(List<BuildCoder<?>> buildCoders, List<BuildResponder> buildResponders, List<BuildSourceResolver<?>> buildSourceResolvers) {
         super(buildCoders, buildResponders, buildSourceResolvers);
     }
 
     @Override
     public void buildWithChoose(List<Class<BuildCoder<?>>> builderClasses, BuildSource buildSource, OutputStream outputStream, List<Object> persistSources) throws BuildBusException {
 
-
-
-
-
     }
 
     @Override
-    public void buildWithCustomer(List<BuildCoder<?>> builderClasses, BuildSource buildSource, OutputStream outputStream, List<Object> persistSources) throws BuildBusException {
-
-
-
+    public void buildWithCustomer(List<BuildCoder<?>> builders, BuildSource buildSource, OutputStream outputStream, List<Object> persistSources) throws BuildBusException {
 
     }
 
     @Override
     public void build(BuildSource buildSource, OutputStream outputStream, List<Object> persistSources) throws BuildBusException {
-
-
-
-
 
     }
 }
